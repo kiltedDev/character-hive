6f8b1c18e6bc6f607e11e49862d6e0abb2b4965a
Thomas Wilson <twilson.spqr@gmail.com>
2017-10-21 18:36:38 -0400

    cleaned up testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
-  root to: 'home#index'
-  resources :home do
-  end
+  get 'welcome/index'
+
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
