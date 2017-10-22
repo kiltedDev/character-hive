d49630d39d1206b0222b1fe256e6f229f0fcbd00
Thomas Wilson <twilson.spqr@gmail.com>
2017-10-22 11:18:43 -0400

    react up

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,8 @@ class StaticPagesController < ApplicationController
   end
 
   def calculator
+    @point_buys = [10,15,20,25]
+    @point_values = [-4,-2,-1,0,1,2,3,5,7,10,13,17]
+    @stats = [7,8,9,10,11,12,13,14,15,16,17,18]
   end
 end
