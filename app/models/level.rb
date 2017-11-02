dbf24bcfcbb8aff1f36eeea80b6af6ad509c54db
Thomas Wilson <twilson.spqr@gmail.com>
2017-11-02 14:26:51 -0400

    character tests green

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,2 +1,7 @@
 class Level < ApplicationRecord
+  belongs_to :character
+  belongs_to :character_class
+
+  validates_presence_of :level_number
+  validates_inclusion_of :level_number, in: [*1..20]
 end
--- spec/models/level_spec.rb
+++ spec/models/level_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Level, type: :model do
-  context "#character_level" do
-    it { should have_valid(:character_level).when(1, 8, 12, 20) }
-    it { should_not have_valid(:character_level).when(nil, "", -5, 21, 40) }
+  context "#level_number" do
+    it { should have_valid(:level_number).when(1, 8, 12, 20) }
+    it { should_not have_valid(:level_number).when(nil, "", -5, 21, 40) }
   end
 
   it { should belong_to :character }
