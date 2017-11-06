c59800785ffd35f86912484d2c6265622aded6d6
Thomas Wilson <twilson.spqr@gmail.com>
2017-11-06 11:38:06 -0500

    cleaned up react

--- app/javascript/packs/react-application.js
+++ app/javascript/packs/react-application.js
@@ -1,6 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
 import PBCalc from '../bundles/Calculator/components/PBCalc';
-import CharacterIndexPage from '../bundles/Calculator/components/CharacterIndexPage';
+import CharacterIndexPage from '../bundles/Characters/components/CharacterIndexPage';
 
 ReactOnRails.register({ CharacterIndexPage, PBCalc })
