7672580f84e5f5c2fd33bdd257d453a3b5a09d7e
Thomas Wilson <twilson.spqr@gmail.com>
2017-11-14 23:15:25 -0500

    tests written

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104231740) do
+ActiveRecord::Schema.define(version: 20171115033308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 20171104231740) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "race_name", default: "Human", null: false
+  end
+
+  create_table "details_panels", force: :cascade do |t|
+    t.string "player_name", null: false
+    t.string "campaign"
+    t.string "gender"
+    t.string "alignment", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "levels", force: :cascade do |t|
