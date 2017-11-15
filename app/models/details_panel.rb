0f76267b94a09b4399dd20d1879a5982c241044a
Thomas Wilson <twilson.spqr@gmail.com>
2017-11-14 23:18:50 -0500

    all tests green

--- app/models/details_panel.rb
+++ app/models/details_panel.rb
@@ -1,2 +1,5 @@
 class DetailsPanel < ApplicationRecord
+  validates_presence_of :player_name
+  validates_inclusion_of :alignment, in: ["Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral", "True Neutral", "Chaotic Neutral", "Lawful Evil", "Neutral Evil", "Chaotic Evil"]
+
 end
