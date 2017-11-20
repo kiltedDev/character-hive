0f48867e1e3f9906658d8de16077a3a10c1193e0
Thomas Wilson <twilson.spqr@gmail.com>
2017-11-19 23:24:43 -0500

    all tests green, again

--- app/models/size_category.rb
+++ app/models/size_category.rb
@@ -5,9 +5,14 @@ class SizeCategory < ApplicationRecord
   validates_presence_of :creature_size
   validates_inclusion_of :creature_size, in: ["Fine", "Diminutive", "Tiny", "Small", "Medium", "Large", "Huge", "Gargantuan", "Colossal", "Large (Tall)", "Huge (Tall)", "Gargantuan (Tall)", "Colossal (Tall)"]
   validates_presence_of :size_modifier
+  validates_inclusion_of :size_modifier, in: [8,4,2,1,0,-1,-2,-4,-8]
   validates_presence_of :special_size_modifier
+  validates_inclusion_of :special_size_modifier, in: [-8,-4,-2,-1,0,1,2,4,8]
   validates_presence_of :fly_modifier
+  validates_inclusion_of :fly_modifier, in: [-8,-6,-4,-2,0,2,4,6,8]
   validates_presence_of :stealth_modifier
+  validates_inclusion_of :stealth_modifier, in: [-16,-12,-8,-4,0,4,8,12,16]
   validates_presence_of :base_reach
+  validates_inclusion_of :base_reach, in: [0,5,10,15,20,30]
 
 end
